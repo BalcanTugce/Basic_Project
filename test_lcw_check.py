d78222177b25a4cf34293e476bc5a626e1bde7a4
BalcanTugce <tugce.balcan@useinsider.com>
2023-01-09 15:47:58 +0300

    Ödev6

--- test_lcw_check.py
+++ test_lcw_check.py
@@ -4,15 +4,17 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 
 
 class TestCheckLcwLocators(unittest.TestCase):
     baseUrl = "https://www.lcwaikiki.com/tr-TR/TR"
-    Menu = (By.LINK_TEXT, "KADIN")
-    Sub_Cat = (By.LINK_TEXT, "Pijama Takımı")
+
+    MENU = (By.LINK_TEXT, "KADIN")
+    SUB_CAT = (By.LINK_TEXT, "Pijama Takımı")
     PRODUCT = (By.CLASS_NAME, "product-card__product-info")
-    SIZE = (By.LINK_TEXT, "M")
+    SIZE = (By.CSS_SELECTOR, "#option-size a:not([data-stock*='0']")
     ADD_TO_CART_BUTTON = (By.LINK_TEXT, "SEPETE EKLE")
     CART_ITEM_COUNT = (By.CLASS_NAME, "badge-circle")
     HOME_PAGE_LOGO = (By.CLASS_NAME, "main-header-logo")
@@ -27,25 +29,28 @@ class TestCheckLcwLocators(unittest.TestCase):
         self.driver.implicitly_wait(15)
 
     def test_lcw_check(self):
-        assert self.baseUrl == self.driver.current_url
+        self.driver.get(self.baseUrl)
+        self.assertIn('LC Waikiki', self.driver.title)
 
-        kadin = self.driver.find_element(*self.Menu)
+        kadin = self.driver.find_element(*self.MENU)
         hover = ActionChains(self.driver).move_to_element(kadin)
         hover.perform()
 
-        self.driver.find_element(*self.Sub_Cat).click()
-        assert "Kadın Pijama" == self.driver.title.split(",")[0]
+        self.driver.find_element(*self.SUB_CAT).click()
+        self.assertIn('Kadın Pijama', self.driver.title.split(",")[0])
 
-        self.driver.find_elements(*self.PRODUCT)[1].click()
-        assert "urun" in self.driver.current_url
+        self.driver.find_elements(*self.PRODUCT)[2].click()
+        self.assertTrue(self.wait.until(
+            EC.element_to_be_clickable(self.ADD_TO_CART_BUTTON)).text)
 
         self.driver.find_element(*self.SIZE).click()
         self.driver.find_element(*self.ADD_TO_CART_BUTTON).click()
+
         self.driver.find_element(*self.CART_ITEM_COUNT).click()
-        assert "sepetim" in self.driver.current_url
+        self.assertEqual("https://www.lcwaikiki.com/tr-TR/TR/sepetim", self.driver.current_url)
 
         self.driver.find_element(*self.HOME_PAGE_LOGO).click()
-        assert self.baseUrl == self.driver.current_url
+        self.assertIn('LC Waikiki', self.driver.title)
 
     def tearDown(self):
         self.driver.close()
